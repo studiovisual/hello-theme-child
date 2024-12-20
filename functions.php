1db6e25b6cbeefc8eccfa91f0722ebae5ba012aa
elopes-sv <elopes@studiovisual.com.br>
2024-12-20 17:20:39 -0300

    feature/sit-288: Atualiza código field de imagem

--- functions.php
+++ functions.php
@@ -189,36 +189,47 @@ add_filter('wp_nav_menu_objects', 'add_menu_description_to_items', 10, 2);
  * @param int $depth Nível de profundidade do menu.
  * @param object $args Argumentos do menu.
  */
-function add_custom_menu_image_field($item_id, $item, $depth, $args) {
-	$image_url = get_post_meta($item_id, '_menu_item_image_url', true);
-
+function add_custom_menu_image_field( $item_id, $item, $depth, $args ) {
+	$image_url = get_post_meta( $item_id, '_menu_item_image_url', true );
 	?>
+
 	<p class="field-menu-item-image-url description description-wide">
-			<label for="edit-menu-item-image-url-<?php echo $item_id; ?>">
-					<?php _e('Image URL'); ?><br>
-					<input type="text" id="edit-menu-item-image-url-<?php echo $item_id; ?>" class="widefat code edit-menu-item-image-url" name="menu-item-image-url[<?php echo $item_id; ?>]" value="<?php echo esc_attr($image_url); ?>" />
+			<label for="edit-menu-item-image-url-<?php echo esc_attr( $item_id ); ?>">
+					<?php echo esc_html( __( 'Image URL' ) ); ?><br>
+					<input
+							type="text"
+							id="edit-menu-item-image-url-<?php echo esc_attr( $item_id ); ?>"
+							class="widefat code edit-menu-item-image-url"
+							name="menu-item-image-url[<?php echo esc_attr( $item_id ); ?>]"
+							value="<?php echo esc_attr( $image_url ); ?>"
+					/>
 					<br />
-					<small><?php _e('Insira o URL da imagem que deseja usar para este item de menu.'); ?></small>
+					<small><?php echo esc_html( __( 'Insira o URL da imagem que deseja usar para este item de menu.' ) ); ?></small>
+					<br />
+					<small><?php echo esc_html( __( 'Obs: Campo ativo apenas para o subitem STRATWS One e subitens de Módulos.' ) ); ?></small>
 			</label>
 	</p>
+
 	<?php
 }
-add_filter('wp_nav_menu_item_custom_fields', 'add_custom_menu_image_field', 10, 4);
+add_filter( 'wp_nav_menu_item_custom_fields', 'add_custom_menu_image_field', 10, 4 );
+
 
 /**
-* Salva o URL da imagem inserido nos itens de menu.
-*
-* @param int $menu_id O ID do menu.
-* @param int $menu_item_db_id O ID do item de menu no banco de dados.
-* @param object $args Argumentos adicionais para a atualização.
-*/
-function save_custom_menu_image_field($menu_id, $menu_item_db_id, $args) {
-	if (isset($_POST['menu-item-image-url'][$menu_item_db_id])) {
-			$image_url = sanitize_text_field($_POST['menu-item-image-url'][$menu_item_db_id]);
-			update_post_meta($menu_item_db_id, '_menu_item_image_url', $image_url);
+ * Salva o URL da imagem inserido nos itens de menu.
+ *
+ * @param int $menu_id O ID do menu.
+ * @param int $menu_item_db_id O ID do item de menu no banco de dados.
+ * @param object $args Argumentos adicionais para a atualização.
+ */
+function save_custom_menu_image_field( $menu_id, $menu_item_db_id, $args ) {
+	if ( isset( $_POST['menu-item-image-url'][ $menu_item_db_id ] ) ) {
+			$image_url = esc_url_raw( $_POST['menu-item-image-url'][ $menu_item_db_id ] );
+			update_post_meta( $menu_item_db_id, '_menu_item_image_url', $image_url );
 	}
 }
-add_action('wp_update_nav_menu_item', 'save_custom_menu_image_field', 10, 3);
+add_action( 'wp_update_nav_menu_item', 'save_custom_menu_image_field', 10, 3 );
+
 
 /**
 * Adiciona uma imagem ao item de menu, se houver um URL de imagem.
