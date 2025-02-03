59c45dd0f421c7ae50c6f76cfbd70736e2dca423
elopes-sv <elopes@studiovisual.com.br>
2025-02-03 13:09:35 -0300

    feature/sit-323: Desenvolvimento Footer

--- template-parts/dynamic-footer.php
+++ template-parts/dynamic-footer.php
@@ -5,164 +5,126 @@
  * @package HelloElementor
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
 
-$is_editor = isset( $_GET['elementor-preview'] );
-$site_name = get_bloginfo( 'name' );
-$tagline   = get_bloginfo( 'description', 'display' );
-$footer_class = did_action( 'elementor/loaded' ) ? hello_get_footer_layout_class() : '';
-$footer_nav_menu = wp_nav_menu( [
-	'theme_location' => 'menu-2',
-	'fallback_cb' => false,
-	'container' => false,
-	'echo' => false,
-] );
+$stylesheet_dir_uri = esc_url(get_stylesheet_directory_uri());
 ?>
-<footer id="site-footer" class="site-footer dynamic-footer <?php echo esc_attr( $footer_class ); ?>">
 
-<div class="footer-session footer-session-1 principal">
-  <div class="column">
-    <?php
-    if ( function_exists( 'the_custom_logo' ) ) {
-        echo get_custom_logo(); // Exibe o logo do site
-    }
-    ?>
-    <p><?php echo esc_html( get_bloginfo( 'description', 'display' ) ); ?></p>
-  </div>
-
-  <?php
-  if ( has_nav_menu( 'primary-footer-menu' ) ) :
-    
-    $menu_items = wp_get_nav_menu_items( get_nav_menu_locations()['primary-footer-menu'] );
-
-    if ( $menu_items ) :
-        $menu_columns = [];
-        
-        foreach ( $menu_items as $item ) {
-            if ( $item->menu_item_parent == 0 ) {
-                
-                $menu_columns[$item->ID] = [
-                    'title' => $item->title,
-                    'url'   => $item->url,
-                    'sub_items' => [],
-                ];
-            } else {
-                
-                $menu_columns[$item->menu_item_parent]['sub_items'][] = [
-                    'title' => $item->title,
-                    'url'   => $item->url,
-                ];
-            }
-        } 
-	?>
+<footer id="sv-footer" class="sv-footer">
+	<div class="sv-footer__container">
+		<div class="sv-footer__main">
+			<div class="sv-footer__content">
+				<a href="<?php echo esc_url(home_url()); ?>" title="<?php esc_attr_e('Ir para a página inicial', 'hello-theme-child'); ?>">
+					<img
+						src="<?php echo $stylesheet_dir_uri; ?>/assets/images/logo-siteware.svg"
+						alt="Logo Studio Visual"
+						class="sv-footer__logo"
+						width="186"
+						height="26"
+						loading="lazy"
+					/>
+				</a>
 	
-	<?php foreach ( $menu_columns as $column ) : ?>
-    <div class="column">
-        <!-- Versão para Desktop -->
-        <details id="details-desktop" open class="footer-menu-item">
-            <summary role="button" aria-expanded="true"><a href="<?php echo esc_url( $column['url'] ); ?>"><?php echo esc_html( $column['title'] ); ?></a></summary>
+				<p class="sv-footer-description">
+					<?php esc_html_e('Levando pessoas e empresas mais longe.', 'hello-theme-child'); ?>
+				</p>
 
-            <?php if ( ! empty( $column['sub_items'] ) ) : ?>
-                <ul>
-                    <?php foreach ( $column['sub_items'] as $sub_item ) : ?>
-                        <li><a href="<?php echo esc_url( $sub_item['url'] ); ?>"><?php echo esc_html( $sub_item['title'] ); ?></a></li>
-                    <?php endforeach; ?>
-                </ul>
-            <?php endif; ?>
-        </details>
+				<?php if (defined('ICL_LANGUAGE_CODE') && ICL_LANGUAGE_CODE === 'pt-br'): ?>
+					<p class="sv-footer__address">
+						<strong>Belo Horizonte</strong> <br>
+						Rua dos Timbiras, 2352 – 5º andar – Lourdes, Belo Horizonte – MG, 30140-069
+					</p>
+				<?php endif; ?>
+			</div>
+		
+			<nav class="sv-footer__menu" aria-label="<?php esc_attr_e('Menu Principal do Rodapé', 'hello-theme-child'); ?>">
+				<?php
+					if (has_nav_menu('primary-footer-menu')) {
+						wp_nav_menu(array(
+							'theme_location' => 'primary-footer-menu',
+							'container'      => false,
+							'menu_class'     => 'sv-footer__menu-list',
+							'depth'          => 2,
+						));
+					}
+				?>
+			</nav>
+		</div>
 
-        <!-- Versão para Mobile -->
-        <details id="details-mobile" class="footer-menu-item">
-            <summary role="button" aria-expanded="false"><?php echo esc_html( $column['title'] ); ?></summary>
+		<div class="sv-footer__certifications">
+			<?php
+				$certifications = [
+					'scale-upv2.webp'     => 'Endeavor Scale Up',
+					'top-scaleupsv2.webp' => 'Top Scaleups',
+					'microsoft.svg'       => 'Microsoft Partner Gold',
+					'cubo.svg'            => 'Cubo Itaú',
+					'criatec.svg'         => 'Criatec 2',
+					'crescera.svg'        => 'Crescera Capital',
+					'great-place.svg'     => 'Great Place to Work'
+				];
 
-            <?php if ( ! empty( $column['sub_items'] ) ) : ?>
-                <ul>
-                    <?php foreach ( $column['sub_items'] as $sub_item ) : ?>
-                        <li><a href="<?php echo esc_url( $sub_item['url'] ); ?>"><?php echo esc_html( $sub_item['title'] ); ?></a></li>
-                    <?php endforeach; ?>
-                </ul>
-            <?php endif; ?>
-        </details>
-    </div>
-<?php endforeach; ?>
-	
-    <?php else :
-        echo '<p>Erro ao carregar os itens do menu de rodapé.</p>';
-    endif;
+				foreach ($certifications as $certification => $alt_text): ?>
+					<img
+						src="<?php echo $stylesheet_dir_uri; ?>/assets/images/certifications/<?php echo esc_attr($certification); ?>"
+						alt="<?php echo esc_attr($alt_text); ?>"
+						width="100"
+						height="65"
+						loading="lazy"
+					>
+			<?php endforeach; ?>
+		</div>
 
-  else :
-    echo '<p>Menu de rodapé não configurado.</p>';
-  endif;
-  ?>
+		<div class="sv-footer__menu-info">
+			<nav class="sv-footer__menu-info-list" aria-label="<?php esc_attr_e('Links Informativos do Rodapé', 'hello-theme-child'); ?>">
+				<?php
+					if (has_nav_menu('footer-menu-info')) {
+						wp_nav_menu(array(
+							'theme_location' => 'footer-menu-info',
+							'container'      => false,
+							'menu_class'     => 'sv-footer__menu-info-list-itens',
+							'depth'          => 2,
+						));
+					}
+				?>
+			</nav>
 
-</div>
-	
-  <section class="footer-session-2">
-<div class="footer-session-2-container">
-  <div class="footer-session-2-column">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo-sup-2021-1440x439-1-1024x312.webp" alt="Endeavor Scale Up">
-  </div>
-  <div class="footer-session-2-column">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/TOP-ScaleUps-black.png.webp" alt="TOP Scaleups">
-  </div>
-  <div class="footer-session-2-column">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Microsoft-partner-06.webp" alt="Microsoft Partner">
-  </div>
-  <div class="footer-session-2-column">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Cubo-05.webp" alt="Cubo">
-  </div>
-  <div class="footer-session-2-column">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Criatec-04.webp" alt="Criatec">
-  </div>
-  <div class="footer-session-2-column">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Crescera-02.webp" alt="Crescera Capital">
-  </div>
-  <div class="footer-session-2-column">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/GPTW-03.webp" alt="Great Place to Work Certification">
-  </div>
-</div>
+			<div class="sv-footer__social-container">
+				<p class="sv-footer__social-text">
+					<?php esc_html_e('Nos siga nas redes sociais:', 'hello-theme-child'); ?>
+				</p>
 
-  </section>
+				<div class="sv-footer__social_icons">
+					<?php
+						$social_icons = [
+							'linkedin'  => 'https://br.linkedin.com/company/siteware',
+							'facebook'  => 'https://pt-br.facebook.com/Siteware/',
+							'instagram' => 'https://www.instagram.com/sitewaresolucoes/',
+							'youtube'   => 'https://www.youtube.com/channel/UCXmB3mcpQIpBp3MBU8ptuZA'
+						];
+					foreach ($social_icons as $platform => $link): ?>
+						<a
+							href="<?php echo esc_url($link); ?>"
+							aria-label="<?php printf(esc_attr__('Visite nosso perfil no %s', 'hello-theme-child'), ucfirst($platform)); ?>"
+							title="<?php echo sprintf(esc_attr__('%s Siteware', 'hello-theme-child'), ucfirst($platform)); ?>"
+							target="_blank"
+							rel="noopener noreferrer">
+							<img
+								src="<?php echo $stylesheet_dir_uri; ?>/assets/images/social-media/<?php echo esc_attr($platform); ?>.svg"
+								alt="<?php echo esc_attr(ucfirst($platform)); ?>"
+								width="24"
+								height="24"
+								loading="lazy"
+							>
+						</a>
+					<?php endforeach; ?>
+				</div>
+			</div>
+		</div>
 
-  <hr class="footer-divider">
-
-  <!-- Sessão 3 -->
-  <div class="footer-session footer-session-3">
-    <div class="footer-session-3-links">
-        <a href="https://siteware.intest.com.br/politicas-de-privacidade/">
-            <?php _e( 'Políticas de privacidade', 'hello-elementor-child' ); ?>
-        </a>
-        <a href="https://app.pipefy.com/public/form/v7kOE2Us">
-            <?php _e( 'Canal de denúncias', 'hello-elementor-child' ); ?>
-        </a>
-        <a href="https://siteware.stratws.com/">
-            <?php _e( 'Acessar o STRATWs One', 'hello-elementor-child' ); ?>
-        </a>
-    </div>
-
-    <div class="footer-session-3-social">
-      <p>Nos siga nas redes sociais</p>
-      <div class="icones-sociais">
-    <a href="https://br.linkedin.com/company/siteware" target="_blank" aria-label="Visite nosso perfil no LinkedIn" title="Linkedin Siteware">
-	  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/social-media/linkedin.svg" alt="Linkedin" width="24" height="24">
-    </a>
-    <a href="https://pt-br.facebook.com/Siteware/" target="_blank" aria-label="Visite nosso perfil no Facebook" title="Facebook Siteware">
-	  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/social-media/facebook.svg" alt="Facebook" width="24" height="24">
-    </a>
-    <a href="https://www.instagram.com/sitewaresolucoes/" target="_blank" aria-label="Visite nosso perfil no Instagram" title="Instagram Siteware">
-	  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/social-media/instagram.svg" alt="Instagram" width="24" height="24">
-    </a>
-    
-    <a href="https://www.youtube.com/c/Siteware" target="_blank" aria-label="Visite nosso perfil no YouTube" title="YouTube Siteware">
-	  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/social-media/youtube.svg" alt="YouTube" width="24" height="24">
-    </a>
-  </div>
-    </div>
-  </div>
-
-  <section class="footer-copyright">
-    <p><?php _e( 'Siteware © 2024 Todos os direitos reservados', 'hello-elementor-child' ); ?></p>
-  </section>
+		<div class="sv-footer__copyright">
+			<?php echo 'Siteware © ' . date('Y') . ' - ' . esc_html__('Todos os direitos reservados​', 'hello-theme-child'); ?>
+		</div>
+	</div>
 </footer>
