8f5637039c5db90d482373ff32932da9df2c46d3
elopes-sv <elopes@studiovisual.com.br>
2025-02-06 14:42:24 -0300

    feature/svms-1: Ajustes deploy

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'HELLO_ELEMENTOR_CHILD_VERSION', '2.0.1' );
+define( 'HELLO_ELEMENTOR_CHILD_VERSION', '2.0.2' );
 
 /**
  * Load child theme scripts & styles.
