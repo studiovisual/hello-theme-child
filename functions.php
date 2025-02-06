ee36e4f3c8fc3614a735c4bbebe60e8a6db81a28
elopes-sv <elopes@studiovisual.com.br>
2025-02-06 15:17:10 -0300

    feature/svms-1: Ajustes PR

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'HELLO_ELEMENTOR_CHILD_VERSION', '2.0.2' );
+define( 'HELLO_ELEMENTOR_CHILD_VERSION', '2.0.3' );
 
 /**
  * Load child theme scripts & styles.
